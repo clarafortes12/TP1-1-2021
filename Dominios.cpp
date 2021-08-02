fbe226d504f24867c10da3d7e69a369720ac1067
shoshou <lemrb1500@gmail.com>
2021-08-02 14:00:04 -0300

    arquivo cpp com os metodos e arquivo cpp com os testes

---
+++ Dominios.cpp
@@ -0,0 +1,63 @@
+#include "Dominios.h"
+#include <iostream>
+#include <string>
+//---------------------------------------------------------------------
+// Inicializacao de atributo estatico.
+
+
+Capacidade::Capacidade(int valor){
+    validar(valor);
+    this->valor = valor;
+}
+
+// Método para validação com lançamento de exceção.
+
+void Capacidade::validar(int valor){
+    if ((valor!=100) && (valor!=200) && (valor!=300) && (valor!=400) && (valor!=500))
+        throw invalid_argument("Argumento invalido.");
+}
+
+// Método para acesso a atributo.
+
+void Capacidade::setValor(int valor) {
+    validar(valor);
+    this->valor = valor;
+}
+
+Horario::Horario(int hora, int minuto){
+    validar(hora, minuto);
+    if (hora<10)
+        if (minuto)
+            this->valor = "0"+to_string(hora)+":"+to_string(minuto);
+        else
+           this->valor = "0"+to_string(hora)+":"+"0"+to_string(minuto);
+    else
+        if (minuto)
+            this->valor = to_string(hora)+":"+to_string(minuto);
+        else
+           this->valor = to_string(hora)+":"+"0"+to_string(minuto);
+}
+
+void Horario::validar(int hora, int minuto){
+    if (hora > 23 || hora < 0)
+        throw invalid_argument("Argumento invalido.");
+    else
+        if (minuto != 00 && minuto != 15 && minuto != 30 && minuto != 45)
+            throw invalid_argument("Argumento invalido.");
+}
+
+// Método para acesso a atributo.
+
+void Horario::setValor(int hora, int minuto) {
+    validar(hora, minuto);
+    if (hora<10)
+        if (minuto)
+            this->valor = "0"+to_string(hora)+":"+to_string(minuto);
+        else
+           this->valor = "0"+to_string(hora)+":"+"0"+to_string(minuto);
+    else
+        if (minuto)
+            this->valor = to_string(hora)+":"+to_string(minuto);
+        else
+           this->valor = to_string(hora)+":"+"0"+to_string(minuto);
+}
---
+++ main.cpp
@@ -0,0 +1,115 @@
+// Exemplo de elementos da sintaxe da linguagem e de documentação usando Doxygen.
+
+// Incluir cabeçalhos.
+
+#include <iostream>
+
+#include "Dominios.h"
+
+using namespace std;
+
+int main()
+{
+    // ------------------------------------------------------------------------
+    // Criar objetos e interagir com objetos.
+
+    Capacidade testar(100);                                    // Criar objeto com valor válido.
+
+    cout << "Valor = " << testar.getValor() << endl;             // Apresentar valor atual de atributo.
+
+    try{
+        testar.setValor(200);                                     // Armazenar valor válido não resulta em lançamento de exceção.
+    }
+    catch(invalid_argument &exp){                           // Capturar exceção da classe invalid_argument, referência para objeto para evitar cópia.
+        cout << "Excecao : " << exp.what() << endl;
+    }
+
+    cout << "Valor = " << testar.getValor() << endl;             // Apresentar valor atual de atributo.
+
+    try{
+        testar.setValor(101);                                    // Tentativa de armazenar valor inválido resulta em lançamento de exceção.
+    }
+    catch(invalid_argument &exp){                           // Capturar exceção da classe invalid_argument, referência para objeto para evitar cópia.
+        cout << "Excecao : " << exp.what() << endl;
+    }
+
+    cout << "Valor = " << testar.getValor() << endl;             // Apresentar valor atual de atributo.
+
+    try{
+        testar.setValor(101);                                    // Tentativa de armazenar valor inválido resulta em lançamento de exceção.
+    }
+    catch(...){                                             // Capturar exceção de qualquer classe.
+        cout << "Excecao : " << endl;
+    }
+
+    cout << "Valor = " << testar.getValor() << endl;             // Apresentar valor atual de atributo.
+
+    // ------------------------------------------------------------------------
+    // Exemplo de aninhamento blocos de código try-catch.
+
+    try{
+
+        try {
+            testar.setValor(101);                                // Tentativa de armazenar valor inválido resulta em lançamento de exceção.
+        }
+        catch(...){                                         // Capturar exceção de qualquer classe.
+            cout << "Tratamento parcial." << endl;
+            throw;                                          // Relançamento de exceção.
+        }
+    }
+    catch (invalid_argument &exp){                          // Capturar exceção da classe invalid_argument, referência para objeto para evitar cópia.
+        cout << "Excecao : " << exp.what() << endl;
+    }
+// ------------------------------------------------------------------------
+
+    Horario testarhorai(10,15);                                    // Criar objeto com valor válido.
+
+    cout << "hora = " << testarhorai.getValor() << endl;             // Apresentar valor atual de atributo.
+
+    try{
+        testarhorai.setValor(00,00);                                     // Armazenar valor válido não resulta em lançamento de exceção.
+    }
+    catch(invalid_argument &exp){                           // Capturar exceção da classe invalid_argument, referência para objeto para evitar cópia.
+        cout << "Excecao : " << exp.what() << endl;
+    }
+
+    cout << "hora = " << testarhorai.getValor() << endl;             // Apresentar valor atual de atributo.
+
+    try{
+        testarhorai.setValor(07,25);                                    // Tentativa de armazenar valor inválido resulta em lançamento de exceção.
+    }
+    catch(invalid_argument &exp){                           // Capturar exceção da classe invalid_argument, referência para objeto para evitar cópia.
+        cout << "Excecao : " << exp.what() << endl;
+    }
+
+    cout << "hora = " << testarhorai.getValor() << endl;             // Apresentar valor atual de atributo.
+
+    try{
+        testarhorai.setValor(400,15);                                    // Tentativa de armazenar valor inválido resulta em lançamento de exceção.
+    }
+    catch(...){                                             // Capturar exceção de qualquer classe.
+        cout << "Excecao : " << endl;
+    }
+
+    cout << "Valor = " << testarhorai.getValor() << endl;             // Apresentar valor atual de atributo.
+
+    // ------------------------------------------------------------------------
+    // Exemplo de aninhamento blocos de código try-catch.
+
+    try{
+
+        try {
+            testarhorai.setValor(20,15);                                // Tentativa de armazenar valor inválido resulta em lançamento de exceção.
+        }
+        catch(...){                                         // Capturar exceção de qualquer classe.
+            cout << "Tratamento parcial." << endl;
+            throw;                                          // Relançamento de exceção.
+        }
+    }
+    catch (invalid_argument &exp){                          // Capturar exceção da classe invalid_argument, referência para objeto para evitar cópia.
+        cout << "Excecao : " << exp.what() << endl;
+    }
+
+
+    return 0;
+}
