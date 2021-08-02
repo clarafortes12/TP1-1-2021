72d3901b689ab883c48f4129c5fb50dd9cc89474
shoshou <lemrb1500@gmail.com>
2021-08-02 13:22:36 -0300

    primeiro arquivo com as duas classes iniciais: capacidade e horario

---
+++ Dominios.h
@@ -0,0 +1,51 @@
+// Exemplo de documentação de classe usando ferramenta Doxygen.
+
+#ifndef DOMINIOS_H_INCLUDED
+#define DOMINIOS_H_INCLUDED
+
+#include <stdexcept>
+
+using namespace std;
+
+///
+/// Padr&atilde;o para representa&ccedil;&atilde;o de cliente.
+///
+/// Regras de formato:
+///
+/// - C&oacute;digo &eacute; v&aacute;lido caso <u>menor</u> ou igual a 5.
+///
+
+
+class Capacidade {
+    private:
+            int valor;
+            void validar(int);
+    public:
+            Capacidade(int valor);
+            void setValor(int valor);
+            int getValor() const;
+
+};
+
+inline int Capacidade::getValor() const{         // Metodo inline e const.
+    return valor;
+}
+
+class Horario {
+    private:
+            int hora;
+            int minunto;
+            string valor;
+            void validar(int hora, int minuto);
+    public:
+            Horario(int hora,int minunto);
+            void setValor(int hora,int minunto);
+            string getValor() const;
+
+};
+
+inline string Horario::getValor() const{         // Metodo inline e const.
+    return valor;
+}
+
+#endif // DOMINIOS_H_INCLUDED
